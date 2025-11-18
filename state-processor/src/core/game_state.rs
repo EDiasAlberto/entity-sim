b96534c1dfb5dbfed7f1531463420eee195b7e85
Ethan Dias Alberto <proxamon1@gmail.com>
2025-11-18 11:39:51 +0000

    feat: construct gamestate

--- state-processor/src/core/game_state.rs
+++ state-processor/src/core/game_state.rs
@@ -6,3 +6,11 @@ pub struct GameState {
     terrain_map: Terrain,
     entity_mgmt: EntityMgmt,
 }
+
+impl GameState {
+    pub fn new() -> GameState {
+        let terrain = crate::core::generate_terrain((1000, 1000, 10), None); 
+        let entities = EntityMgmt::new(200, 200, 400, 400);
+        GameState {terrain_map: terrain, entity_mgmt: entities}
+    }
+}
